7876bbb4d68721d8a93dcd70c5cc1f3bba6744f0
SebastianKru <39736641+SebastianKru@users.noreply.github.com>
2022-06-08 11:00:10 +0200

    added env-var testfunctionality in server.ts

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const  envVariable = rocess.env.MY_ENV_Variable;
+
 async function main() {
   while(true) {
-    console.log('Containers rule! v2');
+    console.log('Containers rule! v2! ' + envVariable + "!");
     await sleep(5000);
   }
 }
