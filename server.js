d895c51d80d7969fa73835139bea5b5f6f218190
SebastianKru <39736641+SebastianKru@users.noreply.github.com>
2022-06-13 08:20:06 +0200

    included health check and express server

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+const express = require("express");
+const app = express();
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const ME = process.env.NAME;
-
 async function main() {
   while(true) {
-    console.log('Containers rule! v2!' + ME);
+    console.log('Containers rule! v2!');
     await sleep(5000);
   }
 }
 
 main();
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+
+
+app.get("sumo/", (req, res, next) => {
+  res.status(200).send("Sumo fighters here!");
+}); 
+
+app.get("health/", (req, res, next) => {
+  res.status(200).send("Healthy");
+}); 
